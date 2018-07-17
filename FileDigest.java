af2acb4c7f078579e5965c0a8dcd7413ea7bd389
rahulalishetty <rahulalishetty1905@gmail.com>
2018-07-17 15:07:45 +0530

    Update FileDigest.java

--- FileDigest.java
+++ FileDigest.java
@@ -3,7 +3,7 @@ import java.io.File;
 public class FileDigest {
     public static void main(String[] args){
         try {
-            String filepath = "/home/zemoso/big_data/wordcount.pig";
+            String filepath = "/home/zemoso/inf-sec/message.txt";
             File file = new File(filepath);
             String sha1Hash = HashGenerator.generateHash(file);
             System.out.println("hash for file:"+sha1Hash);
